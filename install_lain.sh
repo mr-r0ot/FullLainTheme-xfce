6ea1c143ad4cb7e5f92cd5d6af11e2e4e4dfcebb
Nicola <117841475+mr-r0ot@users.noreply.github.com>
2026-09-01 10:46:57 -0700

    Update install_lain.sh

--- install_lain.sh
+++ install_lain.sh
@@ -1,1 +1,27 @@
+chmod +x Set_IconTheme.sh
+sudo ./Set_IconTheme.sh
+
+
+# set fontsystem JetBrains
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+
+# Install WhiteSur GTK Theme
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
+cd WhiteSur-gtk-theme
+chmod +x install.sh
+sudo ./install.sh --name LainTheme -c dark --alt normal -t purple --scheme nord -o normal --roundedmaxwindow --silent-mode -l
+cd ..
+
+
+
+
+#  --monterey اهر را از Big Sur به سمت macOS Monterey می‌برد.
+
+
+
+
+
+chmod +x Set_Dock.sh
+sudo ./Set_Dock.sh
 
